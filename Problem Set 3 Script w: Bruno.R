387a8404baa5f09c169c2876e29a5846739caa18
Zachary <71046420+ztm76617@users.noreply.github.com>
2020-09-15 15:05:30 -0400

    Add files via upload

---
+++ Problem Set 3 Script w: Bruno.R
@@ -0,0 +1,18 @@
+# Load Data
+anes_timeseries_2016 <- read_dta("data/anes_timeseries_2016.dta")
+
+# Install + load all packages
+install.packages("tidyverse")
+library(tidyverse)
+
+install.packages("ggplot2")
+library(ggplot2)
+
+install.packages("dplyr")
+library(dplyr)
+
+install.packages("haven")
+library(haven)
+
+# Naming objects
+
